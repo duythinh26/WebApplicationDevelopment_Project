cc793369625770c4f992df0432cda49c400e03a7
duythinh26 <pduythinh79@gmail.com>
2022-05-30 19:13:56 +0700

    fix some bugs

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,12 @@
-import react from "react";
+import React from "react";
+import Navbar from "../components/Navbar"
+import Announcement from "../components/Announcement";
 
 const Homepage = () => {
     return (
         <div>
-            Homepage
+            <Announcement/>
+            <Navbar/>
         </div>
     )
 }
