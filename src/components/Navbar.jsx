2f857a87dac27a83127ce2771e405da491139ebd
duythinh26 <pduythinh79@gmail.com>
2022-05-30 20:04:26 +0700

    change className according to BEM

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,22 @@ import "./css/Navbar.css"
 
 const Navbar = () => {
     return (
-        <div className="navbar--container">
-            <div className="navbar--wrapper">
+        <div className="navbar__container">
+            <div className="navbar__wrapper">
                 <div className="one-three navbar--left">
-                    <div className="navbar--language">EN</div>
-                    <div className="navbar--search-container">
-                        <input className="navbar--search-input"/>
+                    <div className="navbar__language">EN</div>
+                    <div className="navbar__search-container">
+                        <input className="navbar__search-input"/>
                         <Search style={{ color: "black", fontSize: 16}}></Search>
                     </div>
                 </div>
                 <div className="one-three navbar--center">
-                    <h1 className="navbar--logo">Daki Shop</h1>
+                    <h1 className="navbar__logo">Daki Shop</h1>
                 </div>
                 <div className="one-three navbar--right">
-                    <div className="navbar--menu-item">Sign In</div>
-                    <div className="navbar--menu-item">Register</div>
-                    <div className="navbar--menu-item">
+                    <div className="navbar__menu-item">Sign In</div>
+                    <div className="navbar__menu-item">Register</div>
+                    <div className="navbar__menu-item">
                         <Badge bagdeContent={2} color="primary">
                             <ShoppingCartOutlined />
                         </Badge>
