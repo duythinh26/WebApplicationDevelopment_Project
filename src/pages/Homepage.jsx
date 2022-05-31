abd5ae996985b89cc4ffab3a4adc6d0535a7a608
duythinh26 <pduythinh79@gmail.com>
2022-05-31 11:26:28 +0700

    update homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar"
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 const Homepage = () => {
     return (
@@ -9,6 +10,7 @@ const Homepage = () => {
             <Announcement/>
             <Navbar/>
             <Slider/>
+            <Categories/>
         </div>
     )
 }
