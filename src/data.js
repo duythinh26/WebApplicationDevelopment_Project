c4bd8afefd35efd8cbf3a1e29900be30d4d837e4
duythinh26 <pduythinh79@gmail.com>
2022-05-31 10:54:59 +0700

    update data

--- src/data.js
+++ src/data.js
@@ -1,23 +1,41 @@
 export const sliderItems = [
+  {
+    id: 1,
+    img: "https://media.istockphoto.com/photos/women-are-shopping-in-the-summer-she-is-using-a-credit-card-and-picture-id921504204?k=20&m=921504204&s=612x612&w=0&h=xURMNuCE3EjAr69RzDEHW3Ss9rw9dgGRrLsjUfZdGCU=",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/photos/autumn-shopping-picture-id1052446368?k=20&m=1052446368&s=612x612&w=0&h=97aGLSL6mPPSAfcpj5oRzdWel1sLigacrAvvnw1n3Vk=",
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://st2.depositphotos.com/2117297/8300/i/950/depositphotos_83000672-stock-photo-happy-girls-shopping-in-winter.jpg",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+  ];
+
+  export const categories = [
     {
       id: 1,
-      img: "https://media.istockphoto.com/photos/women-are-shopping-in-the-summer-she-is-using-a-credit-card-and-picture-id921504204?k=20&m=921504204&s=612x612&w=0&h=xURMNuCE3EjAr69RzDEHW3Ss9rw9dgGRrLsjUfZdGCU=",
-      title: "SUMMER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "f5fafd",
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
     },
     {
       id: 2,
-      img: "https://media.istockphoto.com/photos/autumn-shopping-picture-id1052446368?k=20&m=1052446368&s=612x612&w=0&h=97aGLSL6mPPSAfcpj5oRzdWel1sLigacrAvvnw1n3Vk=",
-      title: "AUTUMN SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fcf1ed",
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
     },
     {
       id: 3,
-      img: "https://st2.depositphotos.com/2117297/8300/i/950/depositphotos_83000672-stock-photo-happy-girls-shopping-in-winter.jpg",
-      title: "WINTER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fbf0f4",
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
     },
   ];
