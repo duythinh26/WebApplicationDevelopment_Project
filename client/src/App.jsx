eb5987f21178ba62e70d0484758773d334d29667
duythinh26 <pduythinh79@gmail.com>
2022-06-02 16:16:38 +0700

    create login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import Homepage from "./pages/Homepage"
+import Homepage from "./pages/Homepage";
 import ProductListPage from "./pages/ProductListPage";
-import ProductPage from "./pages/ProductPage"
+import ProductPage from "./pages/ProductPage";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
-    return <RegisterPage/>
+    return <LoginPage/>
   };
   
   export default App;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import "../components/css/LoginPage.css"
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='login__container'>
+      <div className="login__wrapper">
+        <h1 className="login__title">Sign In</h1>
+        <form action="" className="login__form">
+          <input className='login__input' type="text" placeholder='Username'/>
+          <input className='login__input' type="password" placeholder='Password'/>
+          <button className="login__button">Login</button>
+          <a href="">Forgot Password?</a>
+          <a href="">Create new account</a>
+        </form>
+      </div>
+    </div>
   )
 }
 
