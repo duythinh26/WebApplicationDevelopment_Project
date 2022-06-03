4252d71de2d13ab501f01c682f0c886092f600e9
Linh <linh.pp269@gmail.com>
2022-06-03 15:30:35 +0700

    update cart.js and order.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 
-const app = express();
 dotenv.config();
 
 mongoose
@@ -20,6 +22,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
 
 
 app.listen(process.env.PORT || 5000, () => { 
