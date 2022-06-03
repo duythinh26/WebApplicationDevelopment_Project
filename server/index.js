a2989e64e6b6b1566bfbf65179d26a4a6c876cf7
Linh <linh.pp269@gmail.com>
2022-06-03 14:42:57 +0700

    "create method"

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+
 
 const app = express();
 dotenv.config();
@@ -11,13 +13,15 @@ mongoose
     .connect((process.env.MONGO_URL))
     .then(() => console.log("DB connection successfully!"))
     .catch((err) => {
-        console.log(err)
+        console.log(err);
     });
 
-app.use(express.json())
-app.use("/api/auth", authRoute)
-app.use("/api/users", userRoute)
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
 
 app.listen(process.env.PORT || 5000, () => { 
-    console.log("Backend server is running!")
+    console.log("Backend server is running!");
 })
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,3 +1,25 @@
+const Product = require("../models/Product");
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+} = require("./verifyToken");
+
 const router = require("express").Router();
 
-module.exports = router
+//CREATE
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
