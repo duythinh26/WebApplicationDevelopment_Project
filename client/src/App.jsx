6cbb5e711c9c82f586b01cefb54eab7c0c45767a
duythinh26 <pduythinh79@gmail.com>
2022-06-05 11:33:37 +0700

    update react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,31 @@ import ProductPage from "./pages/ProductPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import Cart from "./pages/Cart";
+import React from "react";
+import Success from "./pages/Success";
+import {
+    BrowserRouter,
+    Route,
+    Navigate,
+    Routes,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
-    return <Cart/>
+    const user = useSelector((state) => state.user.currentUser);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/home" element = { <Homepage/> }/>
+                <Route path="/products/:category" element = { <ProductListPage/> }/>
+                <Route path="/product/:id" element = { <ProductPage/> }/>
+                <Route path="/cart" element = { <Cart/> }/>
+                <Route path="/register" element = { <RegisterPage/> }/>
+                <Route path="/login" element = { <LoginPage/> }/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
   
 export default App;
