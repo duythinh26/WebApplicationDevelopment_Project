3a39c165ff64f509b28753e3a36600a4e5b9f276
duythinh26 <pduythinh79@gmail.com>
2022-06-05 11:07:40 +0700

    fix bugs and change css class name

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./css/CategoryItem.css"
-import { styled } from '@material-ui/core'
 
 const CategoryItem = ({item}) => {
   return (
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -49,7 +49,7 @@ const Cart = () => {
                 <div className="bottom__info">
                     <div className="bottom__product">
                         <div className="bottom__product-detail">
-                            <img className='product__img' src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" alt="" />
+                            <img className='cart__product--img' src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" alt="" />
                             <div id="detail">
                                 <span>JESSIE THUNDER SHOES</span>
                                 <span><b>ID:</b> 93813718293</span>
