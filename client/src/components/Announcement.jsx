89fc27bc6634149c53047e2b465fb2da557fb8a3
duythinh26 <pduythinh79@gmail.com>
2022-06-06 08:11:43 +0700

    use styleComponents to fix css bugs

--- client/src/components/Announcement.jsx
+++ client/src/components/Announcement.jsx
@@ -1,10 +1,22 @@
-import "./css/Announcement.css"
+import styledComponents from "styled-components";
+
+const Container = styledComponents.div`
+  height: 30px;
+  background-color: #008080;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: normal;
+  position: relative;
+`;
 
 const Announcement = () => {
   return (
-    <div className='announcement__container'>
-        6 June. Mid-year sale. Freeship on orders over $50. Buy now!!!
-    </div>
+    <Container>
+        15 June. Mid-June sale. Freeship on orders over $50. Buy now!!!
+    </Container>
   )
 }
 
