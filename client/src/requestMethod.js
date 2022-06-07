7e3141b70ab682253adb681d19e9c6c88d424e35
duythinh26 <pduythinh79@gmail.com>
2022-06-07 09:15:18 +0700

    add more request method

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -4,10 +4,10 @@ const BASE_URL = "http://localhost:5000/api";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOWFlZWY1YWZkZGVlNTAwMzg5NGY4NSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NDUwNzYzOSwiZXhwIjoxNjU0NzY2ODM5fQ.0llCV1jbxaaehCuSLyDBMXmnl6NsocJIB7tM0fPfJLA";
 
 export const publicRequest = axios.create({
-    baseURL:BASE_URL,
+    baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
-    baseURL:BASE_URL,
+    baseURL: BASE_URL,
     headers: { token: `Bearer ${TOKEN}` },
 });
