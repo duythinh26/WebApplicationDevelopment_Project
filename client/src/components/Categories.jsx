f1d40e27d6a9e9c96816a3b1cdd6e866b8889b10
baochien200 <quocbao12c9@gmail.com>
2022-06-08 20:13:53 +0700

    modify categories page

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import "./css/Categories.css";
 import { categories } from "../data";
 import CategoryItem from './CategoryItem';
+import styledComponents from "styled-components";
+
+const Container = styledComponents.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+
+`
 
 const Categories = () => {
   return (
-    <div className='categories__container'>
+    <Container>
         {categories.map(item => (
             <CategoryItem item = {item} key = {item.id}/>
         ))}
-    </div>
+    </Container>
   )
 }
 
