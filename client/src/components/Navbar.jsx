dfaea2a940162ef81a52a224dfef5017a8ccc316
duythinh26 <pduythinh79@gmail.com>
2022-06-09 14:50:26 +0700

    fix navbar css bugs

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,106 +1,105 @@
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import Badge from '@mui/material/Badge';
-import { Link } from "react-router-dom";
-import "./css/Navbar.css";
-import { useSelector } from "react-redux";
 import styledComponents from 'styled-components';
+import "./css/style.css";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const navbar_contain = styledComponents.div`
+const Container = styledComponents.div`
     height: 60px;
-
 `;
 
-const navbar_wrap = styledComponents.div`
+const Wrapper = styledComponents.div`
     padding: 10px 20px;
     display: flex;
     align-items: center;
     justify-content: space-between;
-
 `;
 
-const navbar_language = styledComponents.div`
-    font-size: 14px;
+const Language = styledComponents.div`
+    font-size: 16px;
     cursor: pointer;
 `;
 
 const Left = styledComponents.div`
+    flex: 1;
     display: flex;
     align-items: center;
-
 `;
 
-const navbar_search_contain = styledComponents.div`
+const SearchContainer = styledComponents.div`
     border: 0.5px solid lightgray;
-    display: flex;
-    align-items: center;
     margin-left: 50px;
     padding: 5px;
+    display: flex;
+    align-items: center;
+`;
+
+const SearchInput = styledComponents.input`
+    border: none;   
 `;
 
 const Center = styledComponents.div`
     text-align: center;
-
 `;
 
 const Logo = styledComponents.h1`
-    font-weight: normal;
+    font-weight: bold;
     color: #000;
-
 `;
 
 const Right = styledComponents.div`
+    flex: 1;
     display: flex;
     align-items: center;
     justify-content: flex-end;
 `;
 
-const menu_item = styledComponents.div`
-    font-style: 14px;
+const MenuItem = styledComponents.div`
+    font-size: 16px;
     margin-left: 50px;
     cursor: pointer;
-
 `;
-
 
 const Navbar = () => {
 
     const quantity = useSelector(state => state.cart.quantity);
 
     return (
-        <navbar_contain>
-            <navbar_wrap>
-                <Left className="one-three ">
-                    <navbar_language>EN</navbar_language>
-                    <navbar_search_contain>
-                        <input className="navbar__search-input"/>
-                        <Search style={{ color: "black", fontSize: 16}}></Search>
-                    </navbar_search_contain>
+        <Container>
+            <Wrapper>
+                <Left className="one-three">
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <SearchInput/>
+                        <Search style={{ color: "black", fontSize: 16 }}></Search>
+                    </SearchContainer>
                 </Left>
-                <Center className="one-three ">
-                    <Link to={"/"} className = "homepage" >
+                <Center className="one-three">
+                    <Link to={ "/" } className = "homepage" >
                         <Logo>Daki Shop</Logo>
                     </Link>
                 </Center>
-                <Right className="one-three ">
+                <Right className="one-three">
                     <Link to="/login" className="link">
-                        <menu_item>Sign In</menu_item>
+                        <MenuItem>Sign In</MenuItem>
                     </Link>
                     <Link to="/register" className="link">
-                        <menu_item>Register</menu_item>
+                        <MenuItem>Register</MenuItem>
                     </Link>
-                    <Link to="/logout" className="link">
-                        <menu_item>Logout</menu_item>
-                    </Link>
+                    {/* <Link to="/logout" className="link">
+                        <MenuItem>Logout</MenuItem>
+                    </Link> */}
                     <Link to="/cart" className="link">
-                        <menu_item>
+                        <MenuItem>
                             <Badge badgeContent={ quantity } color="primary">
                                 <ShoppingCartOutlined/>
                             </Badge>
-                        </menu_item>
+                        </MenuItem>
                     </Link>
                 </Right>
-            </navbar_wrap>
-        </navbar_contain>
+            </Wrapper>
+        </Container>
     )
 }
 
