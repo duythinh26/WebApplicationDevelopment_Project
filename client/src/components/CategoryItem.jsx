5fa239e19297d83577ee5233f8ca5c5e6053790d
duythinh26 <pduythinh79@gmail.com>
2022-06-09 15:03:17 +0700

    fix css bugs

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,63 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./css/CategoryItem.css"
 import styledComponents from "styled-components";
+import { Link } from 'react-router-dom';
 
 const Container = styledComponents.div`
-
-    flex: 1;
-    margin: 5px;
-    height: 70vh;
-    position: relative;
-
+  flex: 1;
+  height: 70vh;
+  margin: 5px;
+  position: relative;
 `;
+
+const Image = styledComponents.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Info = styledComponents.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
 const Title = styledComponents.h1`
-
-    color: #fff;
-    margin-bottom: 20px;
-
-`;
-
-const Info_items = styledComponents.div`
-
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  color: #fff;
+  margin-bottom: 20px;
 `;
 
 const Button = styledComponents.button`
-
-    border: none;
-    padding: 10px;
-    background-color: #fff;
-    color: gray;
-    cursor: pointer;
-    font-weight: normal;
-
+  font-weight: normal;
+  border: none;
+  padding: 10px;
+  background-color: #fff;
+  color: #808080;
+  cursor: pointer;
 `;
 
-const img = styledComponents.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-
-`;
 
 const CategoryItem = ({ item }) => {
   return (
     <Container>
       <Link to = { `/products/${ item.cat }` }>
-        <img className='category-item__img' src={item.img} alt="" />
-        <Info_items>
-            <Title>{ item.title }</Title>
-            <Button>Buy Now!!</Button>
-        </Info_items>
+        <Image src={ item.img } alt="" />
+        <Info>
+          <Title>{ item.title }</Title>
+          <Button>Buy Now!!</Button>
+        </Info>
       </Link>
     </Container>
   )
