6555bb03ae1cc4ca8f1fc93b6ac41743d56d6860
duythinh26 <pduythinh79@gmail.com>
2022-06-09 14:53:36 +0700

    fix css bugs

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import "./css/Categories.css";
 import { categories } from "../data";
 import CategoryItem from './CategoryItem';
 import styledComponents from "styled-components";
 
 const Container = styledComponents.div`
-    display: flex;
-    padding: 20px;
-    justify-content: space-between;
-
-`
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Categories = () => {
   return (
     <Container>
-        {categories.map(item => (
-            <CategoryItem item = {item} key = {item.id}/>
-        ))}
+      {categories.map(item => (
+        <CategoryItem item = {item} key = {item.id}/>
+      ))}
     </Container>
   )
 }
