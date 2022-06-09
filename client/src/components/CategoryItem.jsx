e624a00dd51f0b224539f1de928fbaada0ea19ae
baochien200 <quocbao12c9@gmail.com>
2022-06-09 10:05:29 +0700

    modify categoryitem

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,18 +1,65 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./css/CategoryItem.css"
+import styledComponents from "styled-components";
+
+const Container = styledComponents.div`
+
+    flex: 1;
+    margin: 5px;
+    height: 70vh;
+    position: relative;
+
+`;
+const Title = styledComponents.h1`
+
+    color: #fff;
+    margin-bottom: 20px;
+
+`;
+
+const Info_items = styledComponents.div`
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Button = styledComponents.button`
+
+    border: none;
+    padding: 10px;
+    background-color: #fff;
+    color: gray;
+    cursor: pointer;
+    font-weight: normal;
+
+`;
+
+const img = styledComponents.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+`;
 
 const CategoryItem = ({ item }) => {
   return (
-    <div className='category-item__container'>
+    <Container>
       <Link to = { `/products/${ item.cat }` }>
         <img className='category-item__img' src={item.img} alt="" />
-        <div className="category-item__info-container">
-            <h1 className="info__title">{ item.title }</h1>
-            <button className="info__button">Buy Now!!</button>
-        </div>
+        <Info_items>
+            <Title>{ item.title }</Title>
+            <Button>Buy Now!!</Button>
+        </Info_items>
       </Link>
-    </div>
+    </Container>
   )
 }
 
