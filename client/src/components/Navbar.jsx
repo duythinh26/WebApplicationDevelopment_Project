e7196f79a0175f159e70ee3fb698279e2d9bf971
baochien200 <quocbao12c9@gmail.com>
2022-06-09 10:25:32 +0700

    modify navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,63 @@ import Badge from '@mui/material/Badge';
 import { Link } from "react-router-dom";
 import "./css/Navbar.css";
 import { useSelector } from "react-redux";
+import styledComponents from 'styled-components';
+
+const navbar_contain = styledComponents.div`
+    height: 60px;
+
+`;
+
+const navbar_wrap = styledComponents.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+`;
+
+const navbar_language = styledComponents.div`
+    font-size: 14px;
+    cursor: pointer;
+`;
+
+const Left = styledComponents.div`
+    display: flex;
+    align-items: center;
+
+`;
+
+const navbar_search_contain = styledComponents.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 50px;
+    padding: 5px;
+`;
+
+const Center = styledComponents.div`
+    text-align: center;
+
+`;
+
+const Logo = styledComponents.h1`
+    font-weight: normal;
+    color: #000;
+
+`;
+
+const Right = styledComponents.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+
+const menu_item = styledComponents.div`
+    font-style: 14px;
+    margin-left: 50px;
+    cursor: pointer;
+
+`;
 
 
 const Navbar = () => {
@@ -10,40 +67,40 @@ const Navbar = () => {
     const quantity = useSelector(state => state.cart.quantity);
 
     return (
-        <div className="navbar__container">
-            <div className="navbar__wrapper">
-                <div className="one-three navbar--left">
-                    <div className="navbar__language">EN</div>
-                    <div className="navbar__search-container">
+        <navbar_contain>
+            <navbar_wrap>
+                <Left className="one-three ">
+                    <navbar_language>EN</navbar_language>
+                    <navbar_search_contain>
                         <input className="navbar__search-input"/>
                         <Search style={{ color: "black", fontSize: 16}}></Search>
-                    </div>
-                </div>
-                <div className="one-three navbar--center">
+                    </navbar_search_contain>
+                </Left>
+                <Center className="one-three ">
                     <Link to={"/"} className = "homepage" >
-                        <h1 className="navbar__logo">Daki Shop</h1>
+                        <Logo>Daki Shop</Logo>
                     </Link>
-                </div>
-                <div className="one-three navbar--right">
+                </Center>
+                <Right className="one-three ">
                     <Link to="/login" className="link">
-                        <div className="navbar__menu-item">Sign In</div>
+                        <menu_item>Sign In</menu_item>
                     </Link>
                     <Link to="/register" className="link">
-                        <div className="navbar__menu-item">Register</div>
+                        <menu_item>Register</menu_item>
                     </Link>
                     <Link to="/logout" className="link">
-                        <div className="navbar__menu-item">Logout</div>
+                        <menu_item>Logout</menu_item>
                     </Link>
                     <Link to="/cart" className="link">
-                        <div className="navbar__menu-item">
+                        <menu_item>
                             <Badge badgeContent={ quantity } color="primary">
                                 <ShoppingCartOutlined/>
                             </Badge>
-                        </div>
+                        </menu_item>
                     </Link>
-                </div>
-            </div>
-        </div>
+                </Right>
+            </navbar_wrap>
+        </navbar_contain>
     )
 }
 
