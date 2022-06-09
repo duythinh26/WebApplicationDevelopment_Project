6ad33e1121525bfab17c62e66fc816215d3b283f
baochien200 <quocbao12c9@gmail.com>
2022-06-09 10:30:55 +0700

    Modify news

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -1,19 +1,62 @@
 import React from 'react';
 import { Send } from '@material-ui/icons';
 import "./css/News.css"
+import styledComponents from 'styled-components';
+
+const news_contain = styledComponents.div`
+    height: 60vh;
+    background-color: #fcf5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+const Title = styledComponents.h1`
+
+    font-size: 50px;
+    font-weight: normal;
+    margin-bottom: 20px;
+`;
+
+const Desc = styledComponents.div`
+    font-size: 20px;
+    font-weight: 300px;
+    margin-bottom: 20px;
+
+`;
+
+const input_contain = styledComponents.div`
+    width: 50%;
+    height: 40px;
+    background-color: #fff;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+
+`;
+
+const Button = styledComponents.button`
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: #fff;
+    cursor: pointer;
+
+`;
 
 const News = () => {
   return (
-    <div className='news__container'>
-        <h1 className="news__title">News</h1>
-        <div className="news__desc">Get timely updates from your favorite products.</div>
-        <div className="news__input-container">
+    <news_contain>
+        <Title>News</Title>
+        <Desc>Get timely updates from your favorite products.</Desc>
+        <input_contain>
             <input className='email__input' type="text" placeholder='Your email' />
-            <button className="input__button">
+            <Button>
                 <Send/>
-            </button>
-        </div>
-    </div>
+            </Button>
+        </input_contain>
+    </news_contain>
   )
 }
 
