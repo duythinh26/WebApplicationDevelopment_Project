a681852de55d96dabf6cb27f097ad88403cdd15f
baochien200 <quocbao12c9@gmail.com>
2022-06-09 10:07:51 +0700

    Modify footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from '@material-ui/icons';
+import styledEngine from '@mui/styled-engine';
 import React from 'react';
 import styledComponents from 'styled-components';
 import "./css/Footer.css";
@@ -15,17 +16,79 @@ const SocialIcon = styledComponents.div`
     margin-right: 20px;
 `;
 
+const foot_container = styledComponents.div`
+    display: flex;
+`;
+
+const foot_container_left = styledComponents.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+
+`;
+
+const foot_desc = styledComponents.p`
+    margin: 20px 0px;
+
+`;
+
+const social__contain = styledComponents.div`
+    display: flex;
+
+`;
+
+const foot_contain_center = styledComponents.div`
+
+    flex: 1;
+    padding: 20px;
+
+`;
+
+const foot_title = styledComponents.h2`
+    margin-bottom: 30px;
+
+`;
+
+const foot_list = styledComponents.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+
+`;
+
+const foot_list_item = styledComponents.li`
+    width: 50%;
+    margin-bottom: 10px;
+
+`;
+
+const foot_contain_right = styledComponents.div`
+    flex: 1;
+    padding: 20px;
+
+`;
+
+const contact_item = styledComponents.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+
+`;
+
 const Footer = () => {
   return (
-    <div className='footer__container'>
-        <div className="footer__container--left">
+    <foot_container>
+        <foot_container_left>
             <h1 className="footer__logo">Daki Shop</h1>
-            <p className="footer__desc">
+            <foot_desc>
                 There are many variations of passages of Lorem Ipsum available, but
                 the majority have suffered alteration in some form, by injected
                 humour, or randomised words which don't look even slightly believable.
-            </p>
-            <div className="social__container">
+            </foot_desc>
+            <social__contain>
                 <SocialIcon color='3B5999'>
                     <Facebook style={{ cursor: "pointer" }}/>
                 </SocialIcon>
@@ -38,36 +101,36 @@ const Footer = () => {
                 <SocialIcon color='E60023'>
                     <Pinterest style={{ cursor: "pointer" }}/>
                 </SocialIcon>
-            </div>
-        </div>
-        <div className="footer__container--center">
-            <h2 className="footer__title">Useful Links</h2>
-            <ul className="footer__list">
-                <li className="footer__list-item">Home</li>
-                <li className="footer__list-item">Cart</li>
-                <li className="footer__list-item">Man Fashion</li>
-                <li className="footer__list-item">Woman Fashion</li>
-                <li className="footer__list-item">Accessories</li>
-                <li className="footer__list-item">My Account</li>
-                <li className="footer__list-item">Order Tracking</li>
-                <li className="footer__list-item">Wishlist</li>
-                <li className="footer__list-item">Terms</li>
-            </ul>
-        </div>
-        <div className="footer__container--right">
-            <h2 className="footer__title">Contact</h2>
-            <div className="contact__item">
+            </social__contain>
+        </foot_container_left>
+        <foot_contain_center>
+            <foot_title>Useful Links</foot_title>
+            <foot_list>
+                <foot_list_item>Home</foot_list_item>
+                <foot_list_item>Cart</foot_list_item>
+                <foot_list_item>Man Fashion</foot_list_item>
+                <foot_list_item>Woman Fashion</foot_list_item>
+                <foot_list_item>Accessories</foot_list_item>
+                <foot_list_item>My Account</foot_list_item>
+                <foot_list_item>Order Tracking</foot_list_item>
+                <foot_list_item>Wishlist</foot_list_item>
+                <foot_list_item>Terms</foot_list_item>
+            </foot_list>
+        </foot_contain_center>
+        <foot_contain_right>
+            <foot_title>Contact</foot_title>
+            <contact_item>
                 <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
-            </div>
-            <div className="contact__item">
+            </contact_item>
+            <contact_item>
                 <Phone style={{marginRight:"10px"}}/> +1 234 567 890
-            </div>
-            <div className="contact__item">
+            </contact_item>
+            <contact_item>
                 <MailOutline style={{marginRight:"10px"}}/>daki.shop@contact
-            </div>
+            </contact_item>
             <img className='payment__img' src="https://i.ibb.co/Qfvn4z6/payment.png" alt='payment method'/>
-        </div>
-    </div>
+        </foot_contain_right>
+    </foot_container>
   )
 }
 
